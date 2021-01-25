b19dffff583a9298abb8d2348677ff0089a6d8e1
Dmitry <dima1406002@gmail.com>
2021-01-26 02:15:57 +0300

    fixed all bugs except 1024pxw res.

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,7 @@ window.onscroll = function() {
     document.getElementById("navbarScroller").style.top = "-250px";
   }
   prevScrollpos = currentScrollPos;
-}
+}//скроллинг навбара
 
 $(function()
 {
@@ -16,7 +16,7 @@ $(function()
                       {
                           if (!expanded)
                           {
-                              $('#Greeting').animate({'padding-top' : '11em'}, {duration : 175});
+                              $('#Greeting').animate({'padding-top' : '11.4em'}, {duration : 175});
                               $('#horNav').animate({'width': '100%'}, {duration: 750});
                               expanded = true;
                           }
@@ -27,8 +27,21 @@ $(function()
                               expanded = false;
                           }
                       });
-});
+});//сдвиг контента вниз по нажатию кнопки меню
 
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
-})
+})//открытие модального окна по нажатию на кнопку в навбаре, она будет еще и в другом месте
+
+var typed = new Typed('.banner', {
+    strings: ['Креативности', 'Любознательности', 'Гибкости ума'],
+    typeSpeed: 80,
+    backSpeed: 20,
+    backDelay: 1500,
+    startDelay: 1000,
+    fadeIn: true,
+    cursorChar: '_',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+  });
