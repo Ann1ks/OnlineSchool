facc844a5c17957e4837ce26a443bb842f37f89b
Dmitry <dima1406002@gmail.com>
2021-01-26 21:02:29 +0300

    added transition on navbar

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,15 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbarScroller").style.top = "0px";
-  } else {
-    document.getElementById("navbarScroller").style.top = "-250px";
-  }
-  prevScrollpos = currentScrollPos;
-}//скроллинг навбара
+window.addEventListener('scroll', function () {
+            let header = document.querySelector('nav');
+            let windowPosition = window.scrollY > 0;
+            header.classList.toggle('scrolling-active', windowPosition);
+        });
 
-$(function()
-{
-  var expanded = false;
-  $('.navbar-toggler').click(function()
-                      {
-                          if (!expanded)
-                          {
-                              $('#Greeting').animate({'padding-top' : '11.4em'}, {duration : 175});
-                              $('#horNav').animate({'width': '100%'}, {duration: 750});
-                              expanded = true;
-                          }
-                          else
-                          {
-                             $('#Greeting').animate({'padding-top' : '0em'}, {duration: 275});
-                             $('#horNav').animate({'width': '0%'},{duration: 750});
-                              expanded = false;
-                          }
-                      });
-});//сдвиг контента вниз по нажатию кнопки меню
 
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })//открытие модального окна по нажатию на кнопку в навбаре, она будет еще и в другом месте
+
+
 
 var typed = new Typed('.banner', {
     strings: ['Креативности', 'Любознательности', 'Гибкости ума'],
